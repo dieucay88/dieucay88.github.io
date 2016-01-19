d464f5bdf3314012fc9de7cf88cd3541d3eb7a09
vuhoanggiang <dieucay88@gmail.com>
2016-01-19 15:41:24 +0700

    uplai cho dep

--- giang/js/myJS.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by User on 18/01/2016.
- */
-$(document).ready(function () {
-    $(".button-collapse").sideNav();
-
-});
-
---
+++ js/myJS.js
@@ -0,0 +1,8 @@
+/**
+ * Created by User on 18/01/2016.
+ */
+$(document).ready(function () {
+    $(".button-collapse").sideNav();
+
+});
+
